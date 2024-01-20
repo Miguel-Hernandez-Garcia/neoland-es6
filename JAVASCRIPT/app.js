9a051858ecdd6bfce63083f9620a2ad65855b965
Miguel-Hernandez-Garcia <mighergar@gmail.com>
2024-01-20 12:43:22 +0100

    Iteration#6 con 5.6 y 5.7 pendientes

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -442,4 +442,73 @@ textInput.value*/
 
 // EJERCICIO PENDIENTE
 
+/*EJERCICIOS DE ES6
+Iteracion 6.1: Find (Primer ejercicio)*/
 
+/*-------------------------------------------------------------------------
+6.1 Dado el siguiente array, usa .find() para econtrar el número 100.
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+---------------------------------------------------------------------------*/
+
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+const numbersOneHundred = numbers.find(number => number ===100);
+console.log("🚀 ~ numbersOneHundred:", numbersOneHundred);
+
+/*EJERCICIOS DE ES6
+Iteracion 6.2: Find (Segundo ejercicio)*/
+
+/*-------------------------------------------------------------------------
+6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010.
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
+];
+---------------------------------------------------------------------------*/
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
+];
+const findMovies = movies.find(movie => movie.date===2010);
+console.log("🚀 ~ findMovies:", findMovies);
+
+/*EJERCICIOS DE ES6
+Iteracion 6.3: Find (Segundo ejercicio)*/
+
+/*-------------------------------------------------------------------------
+6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 
+'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
+spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación 
+lo queremos meter en la propiedad .mutation del objeto fusionado.
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+---------------------------------------------------------------------------*/
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+
+
+const alienNameFilter = aliens.find(alien => alien.name==="Cucushumushu");
+console.log("🚀 ~ alienNameFilter:", alienNameFilter)
+
+const mutationPorompo = mutations.find(mutation => mutation.name==="Porompompero");
+console.log("🚀 ~ mutationPorompo:", mutationPorompo);
+
+const alienPorompo = {...alienNameFilter, mutation: mutationPorompo};
+console.log("🚀 ~ alienPorompo:", alienPorompo);
